5ce5fef9636d498d612c4a3e63434de144f843fa
tingwei741852 <tingwei741852@gmail.com>
2021-05-21 17:46:19 +0800

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,145 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div>
+        <router-link to="/"><i class="fas fa-search-dollar"></i>報價預測</router-link>
+        <div class='avator'>
+          <el-popover placement="top-start" width="150" trigger="click">
+              <p class='user_title'>您好! 小翔</p>
+              <ul class="UserList">
+                <li><i class="fas fa-sign-out-alt" style="margin-right:15px;"></i>登出</li>
+              </ul>
+              <div class="avator" slot="reference"><i class="fas fa-user-circle fa-2x"></i></div>
+            </el-popover>
+        </div>
+        <ul class='NavList' id="NavList">
+          <li>
+            PREDICTION
+          </li>
+          <li>
+            SETTING
+          </li>
+          <!-- <li>
+            <el-popover placement="top-start" width="150" trigger="click">
+              <p class='user_title'>您好! 小翔</p>
+              <ul class="UserList">
+                <li><i class="fas fa-sign-out-alt" style="margin-right:15px;"></i>登出</li>
+              </ul>
+              <div class="avator" slot="reference"><i class="fas fa-user-circle fa-2x"></i></div>
+            </el-popover>
+          </li> -->
+        </ul>
+      </div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  @font-face {
+    font-family: custom-sans-serif;
+    src: local("Heiti TC"), local("微軟正黑體"), local("Microsoft JhengHei");
+    unicode-range: U+4E00-9FFF;
+  }
 
-#nav {
-  padding: 30px;
+  @font-face {
+    font-family: custom-sans-serif;
+    src: local(Helvetica), local(Segoe UI);
+    unicode-range: U+00-024F;
+  }
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  #app {
+    font-family: custom-sans-serif;
+    //   -webkit-font-smoothing: antialiased;
+    //   -moz-osx-font-smoothing: grayscale;
+    //   text-align: center;
+    //   color: #2c3e50;
+  }
 
-    &.router-link-exact-active {
-      color: #42b983;
+  body {
+    margin: 0
+  }
+
+  #nav {
+    padding: 15px;
+    padding-left: 25px;
+    padding-right: 25px;
+    background-color: #613F75;
+    color: #ffffff;
+    font-size: 20px;
+
+    a {
+      font-weight: bold;
+      color: #ffffff;
+      text-decoration: none;
+
+      &.router-link-exact-active {
+        color: #ffffff;
+      }
+    }
+
+    .NavList {
+      float: right;
+      margin: 0;
+    }
+
+    .NavList li {
+      display: inline-block;
+      margin: 0;
+      padding: 0;
+      margin-right: 15px;
+      cursor: pointer;
+
+      i {
+        color: #ffffff;
+        font-size: 20px;
+      }
     }
   }
-}
+
+  .avator {
+    font-size: 20px;
+    float: right;
+    cursor: pointer;
+    // display: inline-block;
+  }
+
+  .el-dropdown-link {
+    cursor: pointer;
+    color: #ffffff;
+    font-size: 20px;
+    padding: 0;
+  }
+
+  .user_title {
+    font-weight: bold;
+    font-size: 18px;
+    padding-left: 12px;
+  }
+
+  .UserList {
+    padding: 0;
+  }
+
+  .UserList li {
+    list-style-type: none;
+    font-size: 15px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 12px;
+    text-align: left;
+    cursor: pointer;
+    margin: 0;
+    transition-duration: .2s;
+  }
+
+  .UserList li:hover {
+    background: rgba(96, 62, 116, .1);
+    color: rgb(96, 62, 116);
+  }
+
+  .el-popover {
+    padding: 0 !important;
+  }
+
 </style>
---
+++ src/views/Prediction.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="home">
+    <div class="page_title">
+      <h2 class="title_text">報價預測</h2>
+      <div class="title_line"></div>
+    </div>
+    <div class="btn_block">
+      <div class="output_btn">
+        <el-upload class="upload-demo" action="https://jsonplaceholder.typicode.com/posts/" :on-change="handleChange"
+          :file-list="fileList">
+          <el-button type="primary">檔案上傳</el-button>
+          <!-- <div slot="tip" class="el-upload__tip">只能上傳文件，且不超过500kb</div> -->
+        </el-upload>
+      </div>
+      <el-button type="primary" disabled>檔案檢查</el-button>
+      <el-button type="primary" disabled>開始預測</el-button>
+      <el-button type="primary" disabled>下載結果</el-button>
+    </div>
+    <div class="table_block">
+      <el-table :data="tableData" height="250" border style="width: 100%">
+        <el-table-column prop="no" label="#" width="180">
+        </el-table-column>
+        <el-table-column prop="id" label="零件代碼" width="180">
+        </el-table-column>
+        <el-table-column prop="Material" label="標準材質">
+        </el-table-column>
+        <el-table-column prop="specification" label="標準規範">
+        </el-table-column>
+        <el-table-column prop="thick" label="厚">
+        </el-table-column>
+        <el-table-column prop="width" label="寬">
+        </el-table-column>
+        <el-table-column prop="length" label="長">
+        </el-table-column>
+        <el-table-column prop="prediction" label="Prediction">
+        </el-table-column>
+        <el-table-column prop="Upper" label="Upper Bound">
+        </el-table-column>
+        <el-table-column prop="Lower" label="Lower Bound">
+        </el-table-column>
+        <el-table-column prop="standard" label="Standard">
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tableData: []
+    }
+  }
+}
+
+</script>
+<style>
+  .home {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+
+  .page_title {
+    text-align: center;
+  }
+
+  .title_line {
+    width: 35px;
+    border: 2px solid #613F75;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .btn_block {
+    display: inline;
+    /* padding-left: 20px; */
+  }
+
+  .output_btn {
+    display: inline-block;
+    margin-right: 12px;
+  }
+
+  .table_block {
+    margin-top: 15px;
+  }
+
+</style>
